d3ff5e1f6cdf9f31c6a7f006b88b5152e59b544e
Atif Javed <atif@gems.techverx.com>
2016-03-19 01:10:03 +0500

    changes for email

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,4 +77,22 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  #-------------------------------------------------------------
+  config.action_mailer.smtp_settings = {
+    address: "smtp.gmail.com",
+    port: 587,
+    domain: 'gmail.com',
+    authentication: "plain",
+    enable_starttls_auto: true,
+    user_name: "atifjaved.techverx@gmail.com",
+    password: "mytechverx"
+  }
+  # ActionMailer Config
+  config.action_mailer.default_url_options = { :host => 'https://my-test-shop.herokuapp.com/' }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.raise_delivery_errors = true
+  # Send email in production mode.
+  config.action_mailer.perform_deliveries = true
+  #-------------------------------------------------------------
 end
